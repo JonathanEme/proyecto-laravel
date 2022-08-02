be35b73c55b9136c556fe3adf114ba8513280a30
JonathanEme <jonathane.mendola@gmail.com>
2022-08-02 11:30:25 -0300

    volvi a cargar el estilo que no me tomaba

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class UserController extends Controller
 {
-    public function config(){
+    public function config()
+    {
         return view('user.config');
+    }
+
+    public function update(Request $request)
+    {
+        //conseguir usuario identificado
+        $user = \Auth::user();
+        $id = $user->id;
+
+        //validacion del formulario
+        $validate = $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'nickname' => ['required', 'string', 'max:255', 'unique:users,nickname,' . $id],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $id]
+        ]);
+
+        //recoge los datos del formulario
+        $name = $request->input('name');
+        $surname = $request->input('surname');
+        $nickname = $request->input('nickname');
+        $email = $request->input('email');
+
+
+        //asignar nuevos valores al objeto del usuario
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nickname = $nickname;
+        $user->email = $email;
+
+        //subir la imagen
+        $image_path = $request->file('image_path');
+        if ($image_path) {
+            //poner nombre unico a la imagen
+            $image_path_name = time() . $image_path->getClientOriginalName();
+
+            //guardar en la carpeta storage (storage/app/users)
+            Storage::disk('users')->put($image_path_name, File::get($image_path));
+
+            //seteo el nombre de la imagen en el objeto
+            $user->image = $image_path_name;
+        }
+
+
+        //ejecutar consulta y cambios en la base de datos
+        $user->update();
+        return redirect()->route('config')
+            ->with(['message' => 'Usuario actualizado correctamente']);
+    }
+
+    public function getImage($filename){
+        $file = Storage::disk('users')->get($filename);
+        return new Response($file, 200);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,22 +9,19 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    LaraFoto
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -52,23 +49,9 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="">Inicio</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="">Subir imagen</a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
-                                </a>
-
-                                <a class="dropdown-item" href="">
-                                    Mi perfil
-                                </a>
-
-                                <a class="dropdown-item" href="{{ route('config') }}">
-                                    Configuración
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
@@ -81,7 +64,6 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                    
                                 </div>
                             </li>
                         @endguest
